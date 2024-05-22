3a5fd5f2ecedef967c0eef3225c25a392435956c
pawanbenjamin <pawanbenjamin@gmail.com>
2024-05-22 10:17:37 -0500

    Revisiting project, added back env variables as well as created new google oauth client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const session = await getServerSession();
+
+  console.log("session: ", session);
   return (
     <>
       getServerSession Result:{" "}
