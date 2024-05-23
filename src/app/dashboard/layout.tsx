d7457b9732cdc09fcf15b52e01250a2a086ab681
pawanbenjamin <pawanbenjamin@gmail.com>
2024-05-23 08:31:24 -0500

    fixed layout bg

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import AuthButton from "../components/AuthButton";
 import Sidebar from "../components/Sidebar";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div className="flex flex-col h-screen">
-      <nav className="bg-slate-200">
+      <nav className="bg-slate-200 flex flex-row justify-between">
+        <h1>Welcome, {session?.user?.name}</h1>
         <AuthButton />
       </nav>
       <div className="flex flex-row w-full flex-grow">
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,5 @@ import { getServerSession } from "next-auth";
 export default async function Dashboard() {
   const session = await getServerSession();
 
-  return (
-    <div className="bg-slate-100 w-full">
-      <h1>Welcome, {session?.user?.name}</h1>
-    </div>
-  );
+  return <div className="bg-slate-100 w-full">Dashboard Home</div>;
 }
