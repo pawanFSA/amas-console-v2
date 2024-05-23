b3deb0b2a6384d3baa05c222993aefd50fbb85d6
pawanbenjamin <pawanbenjamin@gmail.com>
2024-05-23 09:47:59 -0500

    form accepts all required cohort information, and accepts csv data

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default async function Sidebar() {
   return (
     <div className="bg-slate-300">
       <h2>This is the Sidebar</h2>
+      <Nav />
     </div>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <Link href="/dashboard/addCohort">Add Cohort</Link>
+    </nav>
   );
 }
