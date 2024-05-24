2cc77d6c20c016918e2003d089d5506d4c86166d
pawanbenjamin <pawanbenjamin@gmail.com>
2024-05-24 08:19:10 -0500

    redirected if not logged in

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession();
 
-  if (!session) {
+  if (session === null) {
     redirect("/");
   }
 
